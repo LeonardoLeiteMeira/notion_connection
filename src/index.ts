54e00a8cad6e4a54793f023465c43f75afaa4a8c
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-09-13 20:12:41 -0300

    Add init and shut down functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import express, {Request, Response, NextFunction, RequestHandler} from 'express';
+import express, { Response, Application } from 'express';
+import { Server } from 'http';
 import logMiddleware from './middlewares/logMiddleware';
 import authMiddleware from './middlewares/authMiddleware';
 import CustomRequest from './types/customRequest';
 import userRouter from './modules/userModule';
 import authRouter from './modules/authModule';
+
+var server:Server;
+
+const initializeServer = async ()=>{
+    console.log("Ready to use! ✅\n");
+}
+
+const shutDownServer = async () => {
+    console.log("\nShutting server down...");
+    server.close(()=>{
+        console.log("Finished!👋\n");
+    });
+}
 
 const port = 3000;
 const app = express();
@@ -15,11 +29,14 @@ app.use(logMiddleware);
 app.use('/auth',authRouter);
 app.use('/user',userRouter);
 
-
 app.get("/health", (req:CustomRequest, res:Response)=>{
     return res.status(200).send({"data":"Heath Check - OK!"});
 });
 
-app.listen(port,()=>{
+server = app.listen(port, async ()=>{
     console.log(`Server is running on port ${port}`);
-})
+    await initializeServer();
+});
+
+process.on('SIGINT',shutDownServer);
+process.on('SIGTERM',shutDownServer);
